587fb5b750704706596fe667361bc76e1abdb8d3
Zach Burke <zach@zsburke.com>
2021-03-26 09:58:33 +0000

    added base files and structure to views and components

---
+++ router.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Bio from './views/Bio.vue'
+import Contact from './views/Contact.vue'
+import CV from './views/CV.vue'
+import Blog from './views/Blog.vue'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/bio',
+            name: 'bio',
+            component: Bio
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: Contact
+        },
+        {
+            path: '/CV',
+            name: 'CV',
+            component: CV
+        },
+        {
+            path: '/blog',
+            name: 'blog',
+            component: Blog
+        }
+    ]
+})
